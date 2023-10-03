9fba67b548a62f27f4c2f99cb99bafc8432019e1
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-10-03 10:57:15 +0300

    Add cors middleware

--- app.js
+++ app.js
@@ -1,16 +1,15 @@
 const createError = require("http-errors");
 const express = require("express");
+const cors = require("cors");
 const path = require("path");
 const logger = require("morgan");
+const mongoose = require("mongoose");
 
 const indexRouter = require("./routes/index");
 
 const app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "pug");
-
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
