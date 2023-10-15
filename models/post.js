f26bfa97da6c548911e48af50a96e7e1cdc21ce3
Eddie-Thiiru <eddiewachaga6@gmail.com>
2023-10-15 17:53:38 +0300

    Update post model

--- models/post.js
+++ models/post.js
@@ -6,7 +6,7 @@ const PostSchema = new Schema({
   author: { type: Schema.Types.ObjectId, ref: "User", required: true },
   text: { type: String, minLength: 1, maxLength: 3000, required: true },
   comments: [{ type: Schema.Types.ObjectId, ref: "Comment", required: true }],
-  likes: { type: Number, default: 0 },
+  likes: [{ type: Schema.Types.ObjectId, ref: "User", required: true }],
   timestamp: { type: String },
 });
 
